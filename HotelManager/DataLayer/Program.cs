afe40a70b600740779e1322cc3b4af3c3106786e
Vinayak Rahate <vinayak@bhoomiinfosolutions.com>
2015-12-01 07:43:32 +0530

    new project added

---
+++ HotelManager/DataLayer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataLayer
+{
+    class Program
+    {
+        static void Main(string[] args) {
+        }
+    }
+}
