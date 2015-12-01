8a24cc9c512d6d3a703c32853c75622ea64be6ad
Vinayak Rahate <vinayak@bhoomiinfosolutions.com>
2015-12-01 08:15:33 +0530

    New functality added

--- HotelManager/BusinessLayer/Program.cs
+++ HotelManager/BusinessLayer/Program.cs
@@ -8,7 +8,7 @@ namespace BusinessLayer
     class Program
     {
         static void Main(string[] args) {
-
+            Console.WriteLine("Added New Functinality in report");
         }
 
         public static void getinfo() { 
